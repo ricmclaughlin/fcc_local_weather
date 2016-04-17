1f51d5ccf897c80aac1bb35be78cf0874aafe142
ric mclaughlin <ric@mclaughlin.today>
2016-04-17 12:01:40 -0700

    Update 2016-04-17T19:01:38.569Z

--- src/app/index.constants.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('fccLocalWeather')
-    .constant('WEATHER_KEY', 'ced22edda5ab59a89469af8a36ce60af');
-})();
--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('fccLocalWeather', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap']);
-
-})();
